1e417cb7d1030041289da11ba98803e4bdf108ec
Isiaka Maryam <isiakamaryam457@gmail.com>
2025-06-22 04:53:31 -0700

    index2

---
+++ quiz.js
@@ -0,0 +1,15 @@
+function checkAnswer() {
+    const correctAnswer = "4"
+    const selectedRadio = document.querySelector('input[name="quiz"]:checked');
+const userAnswer = selectedRadio ? selectedRadio.value : null;
+const feedback = document.getElementById("feedback");
+if (userAnswer === correctAnswer) {
+   feedback.textContent = "Correct! Well done.";
+} else {
+    feedback.textContent = "That's incorrect. Try again!";
+}
+}
+
+
+const submitAnswer = document.getElementById("submit-answer")
+submitAnswer.addEventListener("click", checkAnswer);
